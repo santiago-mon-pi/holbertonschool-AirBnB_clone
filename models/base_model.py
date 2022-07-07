f2df1469ae2a328f6efcb37a564c2182b93325b0
Oscar Castro <oscar.castro9617@gmail.com>
2022-07-06 22:19:04 -0500

    Corrects pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -17,14 +17,14 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
-         
+
     def __str__(self):
         class_name = "[{}]".format(type(self).__name__)
         return "{} ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.datetime.now()
-    
+
     def to_dict(self):
         temp_dict = self.__dict__.copy()
         temp_dict["created_at"] = self.created_at.isoformat()
